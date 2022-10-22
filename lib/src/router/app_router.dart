69cadb0fedcb3d145e6f337a63498a66992cde69
BMarx981 <b.marx981@gmail.com>
2022-10-22 10:32:01 -0400

    Update routes to follow the correct path.

--- lib/src/router/app_router.dart
+++ lib/src/router/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:so_very_good/src/editor_feature/editor_view.dart';
 import 'package:so_very_good/src/router/not_found_screen.dart';
+import 'package:so_very_good/src/save_feature/save_page.dart';
 
 enum AppRoute { home, editor }
 
@@ -9,14 +10,16 @@ final goRouter = GoRouter(
   debugLogDiagnostics: false,
   routes: [
     GoRoute(
-      path: 'save',
-      name: AppRoute.home.name,
-      builder: (context, state) =>
-    ),
-    GoRoute(
       path: '/',
       name: AppRoute.editor.name,
       builder: (context, state) => const EditorView(),
+      routes: [
+        GoRoute(
+          path: 'save',
+          name: AppRoute.home.name,
+          builder: (context, state) => const SaveView(),
+        ),
+      ],
     ),
   ],
   errorBuilder: (context, state) => const NotFoundScreen(),
--- lib/src/save_feature/save_page.dart
+++ lib/src/save_feature/save_page.dart
@@ -1,1 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+class SaveView extends ConsumerWidget {
+  const SaveView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Save'),
+        ),
+        body: GridView(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+          ),
+          children: [
+            SizedBox(
+              height: 100,
+              width: 100,
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.grey,
+                ),
+                child: const Text(""),
+              ),
+            ),
+          ],
+        ));
+  }
+}
